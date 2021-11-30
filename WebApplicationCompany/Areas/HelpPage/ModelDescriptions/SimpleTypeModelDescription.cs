630acd5ffab6fb3e747caac449e0a8b93708fc71
Home <Home@DESKTOP-R0EPPSH>
2021-11-30 11:54:15 -0500

    Add project files.

---
+++ WebApplicationCompany/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplicationCompany.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApplicationCompany/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationCompany.WebApiApplication" Language="C#" %>
